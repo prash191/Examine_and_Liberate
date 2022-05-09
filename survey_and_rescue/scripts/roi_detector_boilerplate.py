fb053ece581e6588fbbdcd9aad0b58ca8621095f
prash191 <psprashant191@gmail.com>
2022-05-09 18:05:14 +0530

    ps commit

--- survey_and_rescue/scripts/roi_detector_boilerplate.py
+++ survey_and_rescue/scripts/roi_detector_boilerplate.py
@@ -11,6 +11,7 @@ import pickle
 import copy
 import numpy as np
 import itertools
+import json
 
 class sr_determine_rois():
 	
@@ -19,7 +20,9 @@ class sr_determine_rois():
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/usb_cam/image_rect",Image,self.image_callback)
 		self.img = None
-		self.img_copy = None
+		self.dic = {}
+		ls = []
+		self.cor = []
 
 	# CV_Bridge acts as the middle layer to convert images streamed on rostopics to a format that is compatible with OpenCV
 	def image_callback(self, data):
@@ -37,14 +40,12 @@ class sr_determine_rois():
 	iv)		Implementing a threshold on their area so that only contours that are the size of the cells remain'''
 	def detect_rois(self):
 		# Add your Code here
-		# You may add additional function parameters
-		self.img_copy = self.img
-		cv2.imshow("Detected ROIs", self.img_copy) #A copy is chosen because self.img will be continuously changing due to the callback function
-		cv2.waitKey(100)
+		
+		img_copy = self.img
+		# cv2.imshow("Detected ROIs", img_copy) #A copy is chosen because self.img will be continuously changing due to the callback function
+		# cv2.waitKey(0)
 
-		#grayscale, median blur, sharpen image
-		gray = cv2.cvtColor(self.img_copy, cv2.COLOR_BGR2GRAY)
-		blur = cv2.medianBlur(gray, 5)
+		blur = cv2.medianBlur(img_copy, 5)
 		sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 		sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
 
@@ -53,36 +54,50 @@ class sr_determine_rois():
 		close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
 
 		# Find contours and filter using threshold area
-		cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+		im2, cnts, hierarchy = cv2.findContours(close, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		im2 = cv2.drawContours(im2, cnts, -1, (0,255,0), 3)
+		cv2.imshow('contour', im2)
+		cv2.waitKey(0)
 
-		min_area = 100
-		max_area = 1500
-		image_number = 0
+		ls = []
+		min_area = 3000
+		max_area = 6000
+		
 		for c in cnts:
 			area = cv2.contourArea(c)
 			if area > min_area and area < max_area:
+				# print("i'm here")
+				print("area == ",area)
 				x,y,w,h = cv2.boundingRect(c)
-				ROI = cnts[y:y+h, x:x+w]
-				# ROI = image[y:y+h, x:x+w]
-				cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
-				# cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-				cv2.rectangle(cnts, (x, y), (x + w, y + h), (36,255,12), 2)
-				image_number += 1
-
-		cv2.imshow('sharpen', sharpen)
-		cv2.imshow('close', close)
-		cv2.imshow('thresh', thresh)
-		cv2.imshow('image', cnts)
-		cv2.waitKey()
+				self.cor.append((x,y,w,h))
+		
 
 		# Please understand the order in which OpenCV detects the contours.
 		# Before saving the files you may sort them, so that their order corresponds will the cell order
 		# This will help you greatly in the next part. 
 	def sort_rois(self):
-		# Add your Code here
-		pass
+		self.dic = {}
+		self.cor.sort()
+		img_copy=self.img
+		ls = []
+		clm = ['A','B','C','D','E','F']
+		row = ['1','2','3','4','5','6']
+		for i in range(0,36,6):
+			a = self.cor[i:i+6]
+			a.sort(key = lambda x:x[1])
+			ls.append(a)
+			j=0
+			for A in a:
+				x,y,w,h = A
+				cv2.rectangle(img_copy,(x,y),(x+w,y+h),(0,255,0),3)
+				txt = clm[i/6]+row[j]
+				self.dic.update({txt:A})
+				cv2.putText(img_copy, txt, (x+w/4,y+h/2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255,255), 2, cv2.LINE_AA)
+				cv2.imshow('Contour', img_copy)
 
+				cv2.waitKey(0)
+				j+=1
+		self.cor=ls
 		
 
 	def query_yes_no(self, question, default=None):
@@ -121,7 +136,8 @@ class sr_determine_rois():
 		Refer to the internet to find out more '''
 	def save_rois(self):
 		#Add your code here
-		pass
+		with open('RoIs.json', mode='w') as outfile:
+			json.dump(self.dic, outfile)
 
 	#You may optionally implement this to display the image as it is displayed in the Figure given in the Problem Statement
 	def draw_cell_names(self, img):
@@ -136,11 +152,14 @@ def main(args):	#Sample process flow
 			if r.img is not None:
 				r.detect_rois()
 				if('''No of cells detected is not 36'''):
+					r.sort_rois()
+					r.save_rois()					
 					new_thresh_flag = r.query_yes_no("36 cells were not detected, do you want to change ##Enter tweaks, this is not necessary##?")
-					# if(new_thresh_flag):
-					# 	#Change settings as per your desire
-					# else:
-					# 	continue
+					if(new_thresh_flag):
+						pass
+						#Change settings as per your desire
+					else:
+						continue
 				else:
 					satis_flag = r.query_yes_no("Are you satisfied with the currently detected ROIs?")
 					if(satis_flag):
@@ -148,8 +167,9 @@ def main(args):	#Sample process flow
 						r.save_rois()
 						cv2.destroyAllWindows()
 						break
-					# else:
+					else:
 						#Change more settings
+						pass
 		# r.draw_cell_names(r.img) # Again, this is optional
 	except KeyboardInterrupt:
 		cv2.destroyAllWindows()
